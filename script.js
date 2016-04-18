527d68a0f456340cbf72e5df90cdf04d1c1ff17e
AmaranthNebula <nikitagia@comcast.net>
2016-04-18 14:29:16 -0700

    event handlers: CE, numbers, and decimal done

--- script.js
+++ script.js
@@ -14,18 +14,47 @@ $(document).ready(function() {
    
    
    //event handlers
-   $(".button").click(function(e) {
+   //when the decimal character is clicked
+   $(".decimal").click(function(e){
        e.preventDefault();
-       var value = $(this).html();
-       //value = "23+2";
-         console.log(value);    
-       if (/!\d/g.test(value)) {
-           console.log("NUMBER");
-       }
- 
+       var current = $("#currentInput").text();
+        if (current.length < 13 && current.indexOf(".") === -1) {
+            $("#currentInput").append(".");
+        }
+        
    });
-
+   // when a number is clicked
+   $(".numeral").click(function(e) {
+       e.preventDefault();
+       var current = $("#currentInput").text();
+       var value = $(this).html();
+       // update display with new number 
+       if (current.length < 13) {
+            if (/^0$/.test(current)) {
+                $("#currentInput").text("");
+            }
+            $("#currentInput").append(value);
+            
+       }
+   });
+   //when a mathematical symbol is clicked
+   $(".symbol").click(function(e){
+      e.preventDefault();
+      var value = $(this).html();
+      console.log(value); 
+   });
    
+   //when the clear all is clicked
+      $("#buttonC").click(function(e){
+      e.preventDefault();
+      var value = $(this).html();
+      console.log(value); 
+   });
+   // when the clear current is clicked
+   $("#buttonCE").click(function(e){
+      e.preventDefault();
+      $("#currentInput").text("0");
+   });
    
     
 });
